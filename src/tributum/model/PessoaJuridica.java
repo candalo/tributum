a9c626d1469ac392f20707253676a10c12911f24
candalo <lucassoares@firstsolution.com.br>
2015-09-27 00:41:49 -0300

    Trocar método de validação de CNPJ

--- src/tributum/model/PessoaJuridica.java
+++ src/tributum/model/PessoaJuridica.java
@@ -5,9 +5,6 @@
  */
 package tributum.model;
 
-import java.util.Arrays;
-import java.util.InputMismatchException;
-
 public class PessoaJuridica extends Colaborador{
     
     private String cnpj;
@@ -21,7 +18,7 @@ public class PessoaJuridica extends Colaborador{
     public PessoaJuridica(String nome, String endereco, String telefone,
                           String cnpj, double valorHoraTrabalho) throws Exception{
         super(nome, endereco, telefone);
-        this.cnpj = cnpj;
+        setCnpj(cnpj);
         this.valorHoraTrabalho = valorHoraTrabalho;
     }
     
@@ -38,76 +35,57 @@ public class PessoaJuridica extends Colaborador{
     
     private boolean isCnpj(String cnpj) {
         
-        // Explode a string e remove os caracteres . e -
-        String[] cnpjAux = cnpj.split(".|-");
-        
-        // Converte o array de strings em uma string
-        cnpj = Arrays.toString(cnpjAux);
-        
-        /* - CNPJ nao pode ser formado por uma sequencia de digitos iguais
-         * - CNPJ nao pode ter o comprimento diferente de 14
-         */
-        if (cnpj.equals("00000000000000") || cnpj.equals("11111111111111") ||
-            cnpj.equals("22222222222222") || cnpj.equals("33333333333333") ||
-            cnpj.equals("44444444444444") || cnpj.equals("55555555555555") ||
-            cnpj.equals("66666666666666") || cnpj.equals("77777777777777") ||
-            cnpj.equals("88888888888888") || cnpj.equals("99999999999999") ||
-           (cnpj.length() != 14)) {
-            return (false);
-        }
-
-        char dig13, dig14;
-        int sm, i, r, num, peso;
-
-        try {
-            
-            // Calculo do primeiro digito verificador
-            sm = 0;
-            peso = 2;
-            for (i = 11; i >= 0; i--) {
-                num = (int) (cnpj.charAt(i) - 48);
-                sm = sm + (num * peso);
-                peso = peso + 1;
-                if (peso == 10) {
-                    peso = 2;
-                }
-            }
-
-            r = sm % 11;
-            if ((r == 0) || (r == 1)) {
-                dig13 = '0';
-            } else {
-                dig13 = (char) ((11 - r) + 48);
-            }
-
-            // Calculo do segundo digito verificador
-            sm = 0;
-            peso = 2;
-            for (i = 12; i >= 0; i--) {
-                num = (int) (cnpj.charAt(i) - 48);
-                sm = sm + (num * peso);
-                peso = peso + 1;
-                if (peso == 10) {
-                    peso = 2;
-                }
-            }
-
-            r = sm % 11;
-            if ((r == 0) || (r == 1)) {
-                dig14 = '0';
-            } else {
-                dig14 = (char) ((11 - r) + 48);
-            }
-
-            // Verifica se os digitos calculados conferem com os digitos informados.
-            if ((dig13 == cnpj.charAt(12)) && (dig14 == cnpj.charAt(13))) {
-                return (true);
-            } else {
-                return (false);
-            }
-        } catch (InputMismatchException erro) {
-            return (false);
-        }
+        if (!cnpj.substring(0, 1).equals("")) {  
+            try {  
+                cnpj = cnpj.replace('.', ' ');//onde há ponto coloca espaço  
+                cnpj = cnpj.replace('/', ' ');//onde há barra coloca espaço  
+                cnpj = cnpj.replace('-', ' ');//onde há traço coloca espaço  
+                cnpj = cnpj.replaceAll(" ", "");//retira espaço  
+                int soma = 0, dig;  
+                String cnpj_calc = cnpj.substring(0, 12);  
+  
+                if (cnpj.length() != 14) {  
+                    return false;  
+                }  
+                char[] chr_cnpj = cnpj.toCharArray();  
+                /* Primeira parte */  
+                for (int i = 0; i < 4; i++) {  
+                    if (chr_cnpj[i] - 48 >= 0 && chr_cnpj[i] - 48 <= 9) {  
+                        soma += (chr_cnpj[i] - 48) * (6 - (i + 1));  
+                    }  
+                }  
+                for (int i = 0; i < 8; i++) {  
+                    if (chr_cnpj[i + 4] - 48 >= 0 && chr_cnpj[i + 4] - 48 <= 9) {  
+                        soma += (chr_cnpj[i + 4] - 48) * (10 - (i + 1));  
+                    }  
+                }  
+                dig = 11 - (soma % 11);  
+                cnpj_calc += (dig == 10 || dig == 11) ? "0" : Integer.toString(  
+                        dig);  
+                /* Segunda parte */  
+                soma = 0;  
+                for (int i = 0; i < 5; i++) {  
+                    if (chr_cnpj[i] - 48 >= 0 && chr_cnpj[i] - 48 <= 9) {  
+                        soma += (chr_cnpj[i] - 48) * (7 - (i + 1));  
+                    }  
+                }  
+                for (int i = 0; i < 8; i++) {  
+                    if (chr_cnpj[i + 5] - 48 >= 0 && chr_cnpj[i + 5] - 48 <= 9) {  
+                        soma += (chr_cnpj[i + 5] - 48) * (10 - (i + 1));  
+                    }  
+                }  
+                dig = 11 - (soma % 11);  
+                cnpj_calc += (dig == 10 || dig == 11) ? "0" : Integer.toString(  
+                        dig);  
+                return cnpj.equals(cnpj_calc);  
+            }  
+            catch (Exception e) {  
+                return false;  
+            }  
+        }  
+        else {  
+            return false;  
+        }  
     }
     
     public double getValorHoraTrabalho() {
