ab5f66395a2a29347b47a1c59eae332083084343
Phillip1Alisson <phill.volpi@gmail.com>
2015-11-23 20:29:42 -0200

    Criar classe Imposto

---
+++ src/tributum/model/Imposto.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tributum.model;
+
+/**
+ *
+ * @author medabot
+ */
+public class Imposto {
+    private String nomeImposto;
+    private double valorImposto;
+
+    public String getNomeImposto() {
+        return nomeImposto;
+    }
+
+    public void setNomeImposto(String nomeImposto) {
+        this.nomeImposto = nomeImposto;
+    }
+
+    public double getValorImposto() {
+        return valorImposto;
+    }
+
+    public void setValorImposto(double valorImposto) {
+        this.valorImposto = valorImposto;
+    }
+    
+    
+    
+}
