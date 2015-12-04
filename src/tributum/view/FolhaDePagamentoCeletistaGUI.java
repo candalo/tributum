44a7849a715ea0ac0545f256e2288e5e54be0d97
Phillip1Alisson <phill.volpi@gmail.com>
2015-12-04 11:34:55 -0200

    Modificar, acrescentado um campo Total de impostos na view FolhaDePagamentoCeletistaGUI

--- src/tributum/view/FolhaDePagamentoCeletistaGUI.java
+++ src/tributum/view/FolhaDePagamentoCeletistaGUI.java
@@ -44,6 +44,8 @@ public class FolhaDePagamentoCeletistaGUI extends javax.swing.JFrame {
         inssTextField = new javax.swing.JTextField();
         salLiqTextField = new javax.swing.JTextField();
         jButton1 = new javax.swing.JButton();
+        jLabel6 = new javax.swing.JLabel();
+        totalImpostoTextField = new javax.swing.JTextField();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
         setTitle("Folha de Pagamento Celetista");
@@ -110,6 +112,10 @@ public class FolhaDePagamentoCeletistaGUI extends javax.swing.JFrame {
             }
         });
 
+        jLabel6.setText("Total impostos:");
+
+        totalImpostoTextField.setEnabled(false);
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
@@ -128,17 +134,19 @@ public class FolhaDePagamentoCeletistaGUI extends javax.swing.JFrame {
                                     .addComponent(jLabel4)
                                     .addComponent(jLabel5)
                                     .addComponent(jLabel3)
-                                    .addComponent(jLabel2))
+                                    .addComponent(jLabel2)
+                                    .addComponent(jLabel6))
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                                     .addComponent(salLiqTextField, javax.swing.GroupLayout.DEFAULT_SIZE, 85, Short.MAX_VALUE)
                                     .addComponent(inssTextField)
                                     .addComponent(irffTextField)
-                                    .addComponent(valorBrutoTextField))))
-                        .addContainerGap(148, Short.MAX_VALUE))
+                                    .addComponent(valorBrutoTextField)
+                                    .addComponent(totalImpostoTextField))))
+                        .addContainerGap(144, Short.MAX_VALUE))
                     .addGroup(layout.createSequentialGroup()
                         .addComponent(calcularButton)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 68, Short.MAX_VALUE)
                         .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 116, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addGap(57, 57, 57))))
         );
@@ -153,7 +161,7 @@ public class FolhaDePagamentoCeletistaGUI extends javax.swing.JFrame {
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(calcularButton)
                     .addComponent(jButton1))
-                .addGap(44, 44, 44)
+                .addGap(18, 18, 18)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel2)
                     .addComponent(valorBrutoTextField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
@@ -169,7 +177,11 @@ public class FolhaDePagamentoCeletistaGUI extends javax.swing.JFrame {
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel5)
                     .addComponent(salLiqTextField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addContainerGap(22, Short.MAX_VALUE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel6)
+                    .addComponent(totalImpostoTextField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap(17, Short.MAX_VALUE))
         );
 
         pack();
@@ -260,7 +272,9 @@ public class FolhaDePagamentoCeletistaGUI extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
     private javax.swing.JLabel jLabel5;
+    private javax.swing.JLabel jLabel6;
     public javax.swing.JTextField salLiqTextField;
+    public javax.swing.JTextField totalImpostoTextField;
     public javax.swing.JTextField valorBrutoTextField;
     // End of variables declaration//GEN-END:variables
 }
