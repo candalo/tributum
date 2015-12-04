0b2dc986a4959882cc2b5b9bb247e9cdc0051f5e
Phillip1Alisson <phill.volpi@gmail.com>
2015-12-04 09:32:25 -0200

    Modificar acrescentado métodos Construtores a classe Imposto

--- src/tributum/model/Imposto.java
+++ src/tributum/model/Imposto.java
@@ -13,20 +13,38 @@ public class Imposto {
     private String nomeImposto;
     private double valorImposto;
 
+    public Imposto(String nomeImposto) throws NomeException {
+        this.setNomeImposto(nomeImposto);
+    }
+
+    public Imposto(String nomeImposto, double valorImposto) throws NomeException, Exception{
+        this.setNomeImposto(nomeImposto);
+        this.setValorImposto(valorImposto);
+    }
+
+    public Imposto() {
+    }
+
     public String getNomeImposto() {
         return nomeImposto;
     }
 
-    public void setNomeImposto(String nomeImposto) {
-        this.nomeImposto = nomeImposto;
+    public void setNomeImposto(String nomeImposto) throws NomeException{
+        if(!nomeImposto.isEmpty())
+            this.nomeImposto = nomeImposto;
+        else
+            throw new NomeException();
     }
 
     public double getValorImposto() {
         return valorImposto;
     }
 
-    public void setValorImposto(double valorImposto) {
-        this.valorImposto = valorImposto;
+    public void setValorImposto(double valorImposto) throws Exception{
+        if(valorImposto >= 0)
+            this.valorImposto = valorImposto;
+        else
+            throw new Exception("Valor Imposto inválido");
     }
     
     
